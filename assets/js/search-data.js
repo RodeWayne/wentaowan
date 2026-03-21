df9eb0695d8de89e64ffa2d629f40e8b6ca54121
RodeWayne <1518443450@qq.com>
2026-03-21 12:16:08 +0000

    Deploying to gh-pages from @ RodeWayne/wentaowan@1b644dfda35a8cee6fd5fa1cff18a585a953c03a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My Curriculum Vitae. You can also download the PDF version using the button on the top right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/wentaowan/cv/";
