6fa7ba9c13dc04f3e2266c805c3cb3239fdf230a
RodeWayne <1518443450@qq.com>
2026-03-26 09:03:50 +0000

    Deploying to gh-pages from @ RodeWayne/wentaowan@70849c4e8dba8c576661afd49140c8e438e929fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -537,25 +537,18 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/wentaowan/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=xYGwtHkAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.xiaohongshu.com/user/profile/65131ab20000000023024052", "_blank");
         },
       },{
       id: 'light-theme',
