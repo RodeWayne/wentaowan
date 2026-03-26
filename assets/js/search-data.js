112d750d9f218b5212747838df755736696a2375
RodeWayne <1518443450@qq.com>
2026-03-26 09:54:47 +0000

    Deploying to gh-pages from @ RodeWayne/wentaowan@435cd54331779ec2d63f5488bd6cb511b69a0cec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -530,13 +530,6 @@ ninja.data = [{
           window.open("mailto:%77%61%6E%77%65%6E%74%61%6F%39%33@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
