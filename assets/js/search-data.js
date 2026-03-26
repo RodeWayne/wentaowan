a1deb3a962c4b3a459cb3f4708929027a4268417
RodeWayne <1518443450@qq.com>
2026-03-26 14:03:45 +0000

    Deploying to gh-pages from @ RodeWayne/wentaowan@6e3acbd65e4db3568b257481480666770a016d24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/wentaowan/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/wentaowan/people/";
-          },
         },{id: "nav-submenus",
           title: "submenus",
           description: "",
